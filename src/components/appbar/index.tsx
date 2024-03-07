2b33cd356a2edb75ee981833c4c750204e09a3d7
irshad2k2 <mhdirshad2k2@gmail.com>
2024-03-07 20:24:15 +0530

    Icon updated

--- src/components/appbar/index.tsx
+++ src/components/appbar/index.tsx
@@ -200,19 +200,12 @@ const AppBar = () => {
     <>
       <nav className="bg-white border-gray-200 dark:bg-gray-900">
         <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-          <a
-            href="https://flowbite.com/"
-            className="flex items-center space-x-3 rtl:space-x-reverse"
-          >
-            <img
-              src="../public/android-chrome-192x192.png"
-              className="h-8"
-              alt="Flowbite Logo"
-            />
+          <div className="flex items-center space-x-3 rtl:space-x-reverse">
+            <img src="../public/icon.png" className="h-8" alt="Flowbite Logo" />
             <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
               Sport Sphere
             </span>
-          </a>
+          </div>
           <button
             data-collapse-toggle="navbar-default"
             type="button"
