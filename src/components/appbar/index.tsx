26ae7d926c31fd78b83868d2726d8782ec9aa6b2
irshad2k2 <mhdirshad2k2@gmail.com>
2024-03-07 20:50:04 +0530

    Icon size adjusted

--- src/components/appbar/index.tsx
+++ src/components/appbar/index.tsx
@@ -201,7 +201,11 @@ const AppBar = () => {
       <nav className="bg-white border-gray-200 dark:bg-gray-900">
         <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
           <div className="flex items-center space-x-3 rtl:space-x-reverse">
-            <img src="../public/android-chrome-192x192.png" className="h-8" alt="Sport_sphere" />
+            <img
+              src="/android-chrome-192x192.png"
+              className="h-8"
+              alt="Sport_sphere"
+            />
             <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
               Sport Sphere
             </span>
